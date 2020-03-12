06ae8ad93499580c6399d0facc9d2c3174484c4a
I'veLosTmyMinD <33401425+IveLosTmyMinD@users.noreply.github.com>
2020-03-12 18:51:34 +0300

    added "delete" form

---
+++ Form3.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace cp_pass_manager
+{
+    partial class deleteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.delNameChoise = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btConfirm = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // delNameChoise
+            // 
+            this.delNameChoise.FormattingEnabled = true;
+            this.delNameChoise.Location = new System.Drawing.Point(12, 48);
+            this.delNameChoise.Name = "delNameChoise";
+            this.delNameChoise.Size = new System.Drawing.Size(242, 21);
+            this.delNameChoise.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите название для удаления";
+            // 
+            // btConfirm
+            // 
+            this.btConfirm.Location = new System.Drawing.Point(16, 121);
+            this.btConfirm.Name = "btConfirm";
+            this.btConfirm.Size = new System.Drawing.Size(95, 23);
+            this.btConfirm.TabIndex = 2;
+            this.btConfirm.Text = "Подтвердить";
+            this.btConfirm.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(160, 121);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(94, 23);
+            this.btCancel.TabIndex = 3;
+            this.btCancel.Text = "Отменить";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // deleteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 199);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btConfirm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.delNameChoise);
+            this.Name = "deleteForm";
+            this.Text = "Удаление";
+            this.Load += new System.EventHandler(this.deleteForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btConfirm;
+        private System.Windows.Forms.Button btCancel;
+        public System.Windows.Forms.ComboBox delNameChoise;
+    }
+}
